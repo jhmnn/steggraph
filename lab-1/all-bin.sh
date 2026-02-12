bef8d521723c2eee3075e687d758d7aa9feac08f
jhmnn <jhmnn@yandex.ru>
2026-02-12 17:59:32 +0700

    add report

--- lab-1/all-bin.sh
+++ lab-1/all-bin.sh
@@ -1,15 +1,61 @@
 #!/bin/bash
 
-SETS=3
-IMGS=5
 KS=8
 
 mkdir -p out/img
 
-for ((i=1; i<${SETS}+1; i++)); do
-  for ((j=1; j<${IMGS}+1; j++)) do
-    for ((k=1; k<${KS}+1; k++)) do
-      ./bin/main bin data/set-${i}/${j}.bmp out/img/bin-${i}-${j}-${k}.bmp ${k}
-    done
-  done
+SET=1
+for ((k=1; k<${KS}+1; k++)) do
+  ./bin/main bin data/set-${SET}/1.bmp out/img/bin-${SET}-1-${k}.bmp ${k}
+  ./bin/main bin out/img/enc-src-1-1-${k}.bmp out/img/bin-enc-src-${SET}-1-${k}.bmp ${k}
 done
+for ((k=1; k<${KS}+1; k++)) do
+  ./bin/main bin data/set-${SET}/2.bmp out/img/bin-${SET}-2-${k}.bmp ${k}
+done
+for ((k=1; k<${KS}+1; k++)) do
+  ./bin/main bin data/set-${SET}/4.bmp out/img/bin-${SET}-4-${k}.bmp ${k}
+done
+for ((k=1; k<${KS}+1; k++)) do
+  ./bin/main bin data/set-${SET}/5.bmp out/img/bin-${SET}-5-${k}.bmp ${k}
+done
+for ((k=1; k<${KS}+1; k++)) do
+  ./bin/main bin data/set-${SET}/6.bmp out/img/bin-${SET}-6-${k}.bmp ${k}
+done
+
+SET=2
+for ((k=1; k<${KS}+1; k++)) do
+  ./bin/main bin data/set-${SET}/1.bmp out/img/bin-${SET}-1-${k}.bmp ${k}
+  ./bin/main bin out/img/enc-src-1-1-${k}.bmp out/img/bin-enc-src-${SET}-1-${k}.bmp ${k}
+done
+for ((k=1; k<${KS}+1; k++)) do
+  ./bin/main bin data/set-${SET}/2.bmp out/img/bin-${SET}-2-${k}.bmp ${k}
+done
+for ((k=1; k<${KS}+1; k++)) do
+  ./bin/main bin data/set-${SET}/3.bmp out/img/bin-${SET}-3-${k}.bmp ${k}
+done
+for ((k=1; k<${KS}+1; k++)) do
+  ./bin/main bin data/set-${SET}/4.bmp out/img/bin-${SET}-4-${k}.bmp ${k}
+done
+for ((k=1; k<${KS}+1; k++)) do
+  ./bin/main bin data/set-${SET}/5.bmp out/img/bin-${SET}-5-${k}.bmp ${k}
+done
+
+SET=3
+for ((k=1; k<${KS}+1; k++)) do
+  ./bin/main bin data/set-${SET}/1.bmp out/img/bin-${SET}-1-${k}.bmp ${k}
+  ./bin/main bin out/img/enc-src-1-1-${k}.bmp out/img/bin-enc-src-${SET}-1-${k}.bmp ${k}
+done
+for ((k=1; k<${KS}+1; k++)) do
+  ./bin/main bin data/set-${SET}/4.bmp out/img/bin-${SET}-4-${k}.bmp ${k}
+done
+for ((k=1; k<${KS}+1; k++)) do
+  ./bin/main bin data/set-${SET}/8.bmp out/img/bin-${SET}-8-${k}.bmp ${k}
+done
+for ((k=1; k<${KS}+1; k++)) do
+  ./bin/main bin data/set-${SET}/46.bmp out/img/bin-${SET}-46-${k}.bmp ${k}
+done
+for ((k=1; k<${KS}+1; k++)) do
+  ./bin/main bin data/set-${SET}/95.bmp out/img/bin-${SET}-95-${k}.bmp ${k}
+done
+
+
